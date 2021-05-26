df8774e4cdda5973ef763dfe1ec0967a2d217470
Mickey <keldiyarovamohlaroyim@gmail.com>
2021-05-26 16:46:49 +0300

    second task

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,42 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int [] fill=new int[100];
+        fillArrays(fill);
+
+
+
+
+//        int[] num = {0, 1, 0, 0, 0, 0};
+//        showArray(num);
+
+/*
+        for (int i = 0; i < num.length; i++) {
+
+            if (num[i] == 0) {
+                num[i] = 1;
+            } else {
+                num[i] = 0;
+            }
+        }
+        showArray(num);
+    */}
+    public static void fillArrays(int[] array){
+        for (int i = 0; i < array.length; i++) {
+            array[i]=i+1;
+            System.out.println(array[i]);
+
+        }
+
     }
+
+    public static void showArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println(" ");
+
+    }
+
+
 }
